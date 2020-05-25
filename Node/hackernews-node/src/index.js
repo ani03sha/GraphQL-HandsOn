90fe46ac21f0efaca59f29b3f6f05520044a6808
Anirudh Sharma <anirudh03sharma@gmail.com>
2020-05-25 23:08:18 +0530

    Connected database and server with the Prisma client

--- Node/hackernews-node/src/index.js
+++ Node/hackernews-node/src/index.js
@@ -1,31 +1,23 @@
 const { GraphQLServer } = require('graphql-yoga');
-
-let links = [{
-    id: 'link-0',
-    url: 'www.howtographql.com',
-    description: 'Fullstack tutorial for GraphQL'
-}];
-
-let idCount = links.length;
+const { prisma } = require('./generated/prisma-client')
 
 // These represent the actual implementation of the GraphQL Schema
 const resolvers = {
     Query: {
         info: () => `This is the API of a HackerNews clone`,
-        feed: () => links,
+        feed: () => (root, args, context,info) => {
+            return context.prisma.links()
+        },
     },
 
     Mutation: {
-        post: (parent, args) => {
-            const link = {
-                id: `link-${idCount++}`,
+        post: (root, args, context) => {
+            return context.prisma.createLink({
+                url: args.url,
                 description: args.description,
-                url: args.url
-            }
-            links.push(link)
-            return link
-        }
-    }
+            })
+        },
+    },
 }
 
 // TypeDefs and Resolvers are bundled into the server to define what API operations are accpeted and 
@@ -33,6 +25,7 @@ const resolvers = {
 const server = new GraphQLServer({
     typeDefs: './src/schema.graphql',
     resolvers,
+    context: { prisma },
 });
 
 server.start(() => console.log(`Server is running on http://localhost:4000`));
