03bd36f6e9e3f9e8c615d78c36c96f709ffdf56c
Anirudh Sharma <anirudh03sharma@gmail.com>
2020-05-29 20:16:01 +0530

    Added authoriation to the app

--- Node/hackernews-node/src/index.js
+++ Node/hackernews-node/src/index.js
@@ -1,23 +1,16 @@
 const { GraphQLServer } = require('graphql-yoga');
 const { prisma } = require('./generated/prisma-client')
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const User = require('./resolvers/User')
+const Link = require('./resolvers/Link')
 
 // These represent the actual implementation of the GraphQL Schema
 const resolvers = {
-    Query: {
-        info: () => `This is the API of a HackerNews clone`,
-        feed: () => (root, args, context,info) => {
-            return context.prisma.links()
-        },
-    },
-
-    Mutation: {
-        post: (root, args, context) => {
-            return context.prisma.createLink({
-                url: args.url,
-                description: args.description,
-            })
-        },
-    },
+    Query,
+    Mutation,
+    User,
+    Link
 }
 
 // TypeDefs and Resolvers are bundled into the server to define what API operations are accpeted and 
@@ -25,7 +18,12 @@ const resolvers = {
 const server = new GraphQLServer({
     typeDefs: './src/schema.graphql',
     resolvers,
-    context: { prisma },
+    context: request => {
+        return {
+            ...request,
+            prisma,
+        }
+    },
 });
 
 server.start(() => console.log(`Server is running on http://localhost:4000`));
