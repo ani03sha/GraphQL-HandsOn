95e77fd7beef9115967b1569df48d5629cd31f24
Anirudh Sharma <anirudh03sharma@gmail.com>
2020-06-16 10:53:18 +0530

    Added mutation resolvers

--- Apollo/fullstack-tutorial/start/server/src/index.js
+++ Apollo/fullstack-tutorial/start/server/src/index.js
@@ -2,15 +2,28 @@ const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
 const { createStore } = require('./utils');
 const resolvers = require('./resolvers');
+const isEmail = require('isemail');
 
 const LaunchAPI = require('./datasources/launch');
 const UserAPI = require('./datasources/user');
 
 const store = createStore();
 
+const context = async ({ req }) => {
+    // Auth check on every request
+    const auth = req.headers && req.headers.authorization || '';
+    const email = Buffer.from(auth, 'base64').toString('ascii');
+    if(!isEmail.validate(email)) return { user: null }
+    // Find a user by their email
+    const users = await store.users.findOrCreate({ where: { email } });
+    const user = users && users[0] || null;
+    return { user: { ...user.dataValues } };
+}
+
 const server = new ApolloServer({ 
     typeDefs,
     resolvers,
+    context,
     dataSources: () => ({
         launchAPI: new LaunchAPI(),
         userAPI: new UserAPI({ store })
